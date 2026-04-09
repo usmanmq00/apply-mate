bf74a1f38ceb9442a29fe4054ea2b054486a3603
Usman Mushtaq <112756872+usmanmq00@users.noreply.github.com>
2026-04-09 17:55:45 +0500

    Fetching all jobs api mapping issue fixed

--- src/main/java/com/applyMate/job/controller/JobController.java
+++ src/main/java/com/applyMate/job/controller/JobController.java
@@ -2,17 +2,19 @@ package com.applyMate.job.controller;
 
 import com.applyMate.job.entity.Job;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class JobController {
 
     private List<Job> jobs = new ArrayList<>();
 
-    @GetMapping("/api/jobs")
+    @GetMapping("/jobs")
     public List<Job> getAllJobs() {
         return jobs;
     }
